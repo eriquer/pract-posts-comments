b682b7b101a93e0945b2ef88c70d45b6f35811bd
Eduardo De Riquer Frutos <eduardo.de.riquer.frutos.ext@ing.com>
2020-10-04 21:43:28 +0200

    MongoDB Configuration

---
+++ 8-pract-posts-comments/src/main/java/es/codeurjc/posts/config/MongoDbConfig.java
@@ -0,0 +1,22 @@
+package es.codeurjc.posts.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@EnableReactiveMongoRepositories
+public class MongoDbConfig extends AbstractReactiveMongoConfiguration {
+
+        @Bean
+        public MongoClient mongoClient() {
+            return MongoClients.create();
+        }
+
+        @Override
+        protected String getDatabaseName() {
+            return "posts-comments";
+        }
+}
+
